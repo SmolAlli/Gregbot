dd9274a9828b0c83d3c57d3c6a186b6317d869db
Bennett Fife <bennett.fife@gmail.com>
2025-01-07 18:41:22 -0800

    add logging feature, creates log files for each streamer that uses the bot

--- src/ignore_these_words.py
+++ src/ignore_these_words.py
@@ -1,4 +1,4 @@
 IGNORE_WORDS = ["is", "are", "was", "were", "be", "being", "been", "has", "have", "had", "a", "an", "the", "this", "that", "these",
-                "those", "my", "your", "his", "her", "their", "and", "i", "or", "but", "for", "nor", "so", "yet", "in", "on", "at", "with", "about", "under", "over", "between", "through", "!", "@", "#", "$", "%", "^", "&", "*", "(", ")", "_", "+",
+                "those", "my", "your", "his", "her", "and", "i", "or", "for", "nor", "so", "yet", "in", "on", "at", "with", "about", "under", "over", "through", "!", "@", "#", "$", "%", "^", "&", "*", "(", ")", "_", "+",
                 "{", "}", "[", "]", "|", "\\", ":", ";", "\"", "'", "<", ">",
                 ",", ".", "?", "/", "~", "`", "=", "-", "_"]
